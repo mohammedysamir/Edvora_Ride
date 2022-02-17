3aad69f8483d875e1404662e5ab934c3fd6023d0
Mohammed Yasser <mohammedre4a@gmail.com>
2022-02-17 02:04:44 +0200

    Add: added intent to main activity.

--- app/src/main/java/com/myasser/edvoraride/SplashActivity.kt
+++ app/src/main/java/com/myasser/edvoraride/SplashActivity.kt
@@ -1,11 +1,17 @@
 package com.myasser.edvoraride
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+    private val milliseconds:Long=2500
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        //added post delayed intent to main activity
+        val handler= Handler()
+        handler.postDelayed({startActivity(Intent(this@SplashActivity,MainActivity::class.java))},milliseconds)
     }
 }
