776e6f51011c2b59760c72ca17ff1a474f22ffed
Mohammed Yasser <mohammedre4a@gmail.com>
2022-02-17 19:30:34 +0200

    Refactor: change date object in ride to LocalDateTime object.

--- app/src/main/java/com/myasser/edvoraride/Ride.kt
+++ app/src/main/java/com/myasser/edvoraride/Ride.kt
@@ -1,7 +1,7 @@
 package com.myasser.edvoraride
 
 import java.net.URL
-import java.util.*
+import java.time.LocalDateTime
 
 /**
  * Data class to store Ride information
@@ -12,7 +12,7 @@ import java.util.*
  * state: represents source state of the ride
  * city: represents source city of the ride
  * */
-class Ride(val id:String, private val path:Array<Int>, val date:Date, val map: URL, val state:String, val city:String) {
+class Ride(val id:String, private val path:Array<Int>, val date: LocalDateTime, val map: URL?, val state:String, val city:String) {
 
     fun getPath():Array<Int>{return path}
     fun getOriginStation():Int{return path[0]}
--- app/src/main/java/com/myasser/edvoraride/RideRecyclerView.kt
+++ app/src/main/java/com/myasser/edvoraride/RideRecyclerView.kt
@@ -7,29 +7,32 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class RideRecyclerView(private val mList: ArrayList<Ride>, private val user: User) :
-    RecyclerView.Adapter<RideRecyclerView.ViewHolder>() {
+        RecyclerView.Adapter<RideRecyclerView.ViewHolder>() {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val rideId: TextView = itemView.findViewById(R.id.ride_id)
         val rideOriginStation: TextView = itemView.findViewById(R.id.ride_origin)
         val ridePath: TextView = itemView.findViewById(R.id.ride_path)
         val rideDate: TextView = itemView.findViewById(R.id.ride_date)
         val rideDistance: TextView = itemView.findViewById(R.id.distance)
+        val rideCity: TextView = itemView.findViewById(R.id.city_name)
+        val rideState: TextView = itemView.findViewById(R.id.state_name)
 
         //TODO: don't forget to implement the map
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.ride_recycler_view, parent, false)
+                LayoutInflater.from(parent.context).inflate(R.layout.ride_recycler_view, parent, false)
         )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val mContext = holder.itemView.context
         val ride = mList[position]
-        holder.rideId.text = mContext.getString(R.string.ride_id, ride.id)
-        holder.rideOriginStation.text =
-            mContext.getString(R.string.origin_station, ride.getOriginStation())
+        holder.rideCity.text = ride.city //place city name
+        holder.rideState.text = ride.state //place state name
+        holder.rideId.text = mContext.getString(R.string.ride_id, ride.id) //place ride id
+        holder.rideOriginStation.text = mContext.getString(R.string.origin_station, ride.getOriginStation())
         //create string path from the array of stations
         var finalPath = ""
         for (s in ride.getPath()) finalPath += s.toString()
@@ -38,7 +41,7 @@ class RideRecyclerView(private val mList: ArrayList<Ride>, private val user: Use
 
         //identify our user and calculate minimum distance
         holder.rideDistance.text =
-            mContext.getString(R.string.distance, user.computeMinDistance(ride))
+                mContext.getString(R.string.distance, user.computeMinDistance(ride))
     }
 
     override fun getItemCount(): Int {
